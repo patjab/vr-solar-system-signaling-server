462b9043592f5063a46ee529aab5f9d0caf5cb03
Patrick Abejar <wireless.patrick@gmail.com>
2019-02-26 07:45:09 -0500

    created signaling server for solar system RTC connection

---
+++ server.js
@@ -0,0 +1,47 @@
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ port: 9090 });
+console.log('Server is running');
+
+const roomRoaster = {
+    room1: [],
+    room2: [],
+    room3: [],
+    room4: []
+};
+
+wss.on('connection', (connection) => {
+
+    connection.on('message', (message) => {
+
+        let data;
+        try {
+            data = JSON.parse(message); 
+        } catch (e) { 
+            console.log("Invalid JSON"); 
+            data = {}; 
+        } 
+
+        // switch(data.type) {
+        //     case 'login':
+        //     break;
+        //     case 'offer':
+        //     break;
+        //     case 'answer':
+        //     break;
+        //     case 'candidate':
+        //     break;
+        // };
+
+    });
+
+});
+
+const sendTo = (connection, message) => {
+    connection.send(JSON.stringify(message));
+}
+
+const broadcastToRoom = (room, message) => {
+    room.forEach(connection => {
+        message.send(JSON.stringify(message));
+    });
+}
